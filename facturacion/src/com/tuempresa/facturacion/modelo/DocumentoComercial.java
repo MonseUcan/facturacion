8a36eec82c006099111b8687614bb2a8e1edd9d3
MonseUcan <113946623+MonseUcan@users.noreply.github.com>
2022-10-19 13:50:14 -0500

    se agrego la lección 12, 13, 14, 15 y 16 ( perdon profe pero se me fue subir el commit en la 14)

--- facturacion/src/com/tuempresa/facturacion/modelo/DocumentoComercial.java
+++ facturacion/src/com/tuempresa/facturacion/modelo/DocumentoComercial.java
@@ -1,12 +1,15 @@
 package com.tuempresa.facturacion.modelo;
 
+import java.math.*;
 import java.time.*;
 import java.util.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.*;
 
 import org.openxava.annotations.*;
 import org.openxava.calculators.*;
+import org.openxava.jpa.*;
 
 import com.tuempresa.facturacion.calculadores.*;
 
@@ -14,7 +17,7 @@ import lombok.*;
 
 @Entity @Getter @Setter
 @View(members=
-       "anyo, numero, fecha;"+
+       "anyo, numero, fecha,"+
           "datos{"+
            	"cliente;"+
            	"detalles;"+
@@ -29,23 +32,54 @@ abstract public class DocumentoComercial extends Identificable{
     int anyo;
     
     @Column(length=6)
-    @DefaultValueCalculator(value = CalculadorSiguienteNumeroParaAnyo.class,
-    properties = @PropertyValue(name="anyo"))
+   // @DefaultValueCalculator(value=CalculadorSiguienteNumeroParaAnyo.class, properties = @PropertyValue(name="anyo"))
+    @ReadOnly
     int numero;
     
     @Required
     @DefaultValueCalculator(CurrentLocalDateCalculator.class)
     LocalDate fecha;
     
+    @Stereotype("MEMO")
+    String observaciones;
+    
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @ReferenceView("simple")
     Cliente cliente;
     
     @ElementCollection
-    @ListProperties("producto.numero, producto.descripcion,cantidad, precioPorUnidad, importe")
+    @ListProperties("producto.numero, producto.descripcion,cantidad, precioPorUnidad,"
+    		+"importe+["
+    		+   "documentoComercial.porcentajeIVA,"
+    		+   "documentoComercial.iva,"
+    		+   "documentoComercial.importeTotal"
+    		+ "]"
+    		)
     Collection<Detalle> detalles;
     
-    @Stereotype("MEMO")
-    String observaciones;
+    @DefaultValueCalculator(CalculadorPorcentajeIVA.class)
+    @Digits(integer=2, fraction=0)
+    BigDecimal porcentajeIVA;
     
+    @ReadOnly
+    @Stereotype("DINERO")
+    @Calculation("sum(detalles.importe)*porcentajeIVA/100")
+    BigDecimal iva;
+    
+    @ReadOnly
+    @Stereotype("DINERO")
+    @Calculation("sum(detalles.importe)+iva")
+    BigDecimal importeTotal;
+    
+    @PrePersist
+    private void calcularNumero() {
+    	Query query = XPersistence.getManager().createQuery(
+    		"select max(f.numero) from "+
+    	    getClass().getSimpleName()+
+    	    " f where f.anyo = :anyo");
+        query.setParameter("anyo", anyo);
+        Integer ultimoNumero = (Integer) query.getSingleResult();
+        this.numero = ultimoNumero == null ? 1 : ultimoNumero + 1;
+  
+    }
 }
